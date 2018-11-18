ff60ecd1756861fef1a1914db4e7e4cb8cccd841
Emibog <37766490+Emibog@users.noreply.github.com>
2018-11-18 12:58:50 +0300

    add video apdate

--- js/video.js
+++ js/video.js
@@ -1,6 +1,6 @@
 var src = 'http://vtrahevip.tv/uploads/posts/2018-11/1542234229_2292.jpg'
 var href = 'http://vtrahevip.tv/2018/11/15/dryannaya-devchonka-susy-gala-vereschala-kak-oslica-kogda-ee-dral-nacho-vidal.html'
-var site = 1;
+var site = 0;
 function addVideo(src, href, site) {
 var newel = document.createElement('li');
 var sil = document.createElement('a');
@@ -8,7 +8,7 @@ var imgfo = document.createElement('img');
 sil.appendChild(imgfo);
 newel.appendChild(sil);
 var dob = document.getElementsByTagName('ul')[site];
-dob.appendChild(newel);
+dob.insertBefore(newel, dob.firstChild);
 sil.setAttribute('href', href);
 sil.setAttribute('target', 'blank');
 imgfo.setAttribute('src', src);
