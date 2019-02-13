31c8d278ec221d0024af2686bbe36f1043ce04ea
Emibog <eminov.bogdan@mail.ru>
2019-02-13 17:15:45 +0300

    fix nav

--- publick_site/js/script.js
+++ publick_site/js/script.js
@@ -1,8 +1,8 @@
 var $check = 0;
 
 if ($(window).width() >= 1280){
-
 $(window).scroll(function() {
+	if($(window).scrollTop() > 0){
 	if($check == 0){
 		$('nav').css('background-color', 'black');
 		$('nav').animate({
@@ -18,6 +18,21 @@ $(window).scroll(function() {
 		$('a.contact').css('color', 'white');
 		$check=1;
 	}
+}else {
+	$('nav').css('background-color', 'white');
+	$('nav').animate({
+			height: '-=20',
+			opacity: '+=0.15'
+		}, 300);
+	$('ul.ul_nav').animate({
+			marginTop: '-=10',
+			marginLeft: '+=37%'
+		}, 300);
+		$('a.home').css('color', 'black');
+		$('a.my_work').css('color', 'black');
+		$('a.contact').css('color', 'black');
+		$check = 0
+}
 })
 
 }
